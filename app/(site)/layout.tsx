6117acaf821b9c8eddb6de77b812c14c851a5d5b
ministrov <61511030+ministrov@users.noreply.github.com>
2025-05-25 13:01:58 +0300

    refactore - Change structure of the auth folder to proper one

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
+import { DM_Sans } from 'next/font/google'
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const dmSans = DM_Sans({
+  weight: ['400', '500', '700'],
   subsets: ['latin'],
-});
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -23,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={dmSans.className}>
+      <body className={'home-page'}>
         {children}
       </body>
     </html>
