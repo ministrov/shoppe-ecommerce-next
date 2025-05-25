f87d2cc319457410cd335940ea92caff32853f3c
ministrov <61511030+ministrov@users.noreply.github.com>
2025-05-25 10:58:48 +0300

    feature - Start creating an arcitecture for all project

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,6 +4,8 @@ import Circle from '@/components/circle/Circle';
 import styles from './page.module.css';
 
 export default function Home() {
+  // const my_little_array = new Array(10).fill('1');
+  // console.log(my_little_array.);
   return (
     <div className={styles.page}>
       <Profile name={'Anton'} text={'Hello World!'} />
