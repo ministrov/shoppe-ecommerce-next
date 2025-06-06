7e7c4089242868a1e3d40f049870032ae770ecc2
Zhila <ministrov2018@gmail.com>
2025-06-06 10:29:16 +0300

    feature - Starting to create a successMessage component

---
+++ components/succesMessage/SuccessMessage.tsx
@@ -0,0 +1,9 @@
+import styles from './SuccesMessage.module.css';
+
+const SuccessMessage = () => {
+  return (
+    <div className={styles.success}>SuccessMessage</div>
+  );
+};
+
+export default SuccessMessage;
