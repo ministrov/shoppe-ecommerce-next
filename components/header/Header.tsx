3e700759cd839ec251957ba13c8da863b0e4f8a0
Zhila <ministrov2018@gmail.com>
2025-06-07 11:01:02 +0300

    feature - Create a markup for header component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import Logo from '../logo/Logo';
 import styles from './Header.module.css';
 
@@ -5,6 +7,37 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <Logo/>
+
+      <div className={styles.mainMenu}>
+        <ul>
+          <li>
+            <Link href={'#'}>Магазин</Link>
+          </li>
+          <li>
+            <Link href={'#'}>О нас</Link>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <Link href={''}>
+              <Image src={'/search.svg'} width={20} height={20} alt={''} />
+            </Link>
+          </li>
+          <li>
+            <Link href={''}>
+            <Image src={'/shopping-cart.svg'} width={20} height={20} alt={''} /></Link>
+          </li>
+          <li>
+            <Link href={''}>
+            <Image src={'/heart.svg'} width={20} height={20} alt={''} /></Link>
+          </li>
+          <li>
+            <Link href={''}>
+            <Image src={'/account.svg'} width={20} height={20} alt={''} /></Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
