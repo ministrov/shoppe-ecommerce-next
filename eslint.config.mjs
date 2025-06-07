7ee799c9a58dec8a1f4dd2f36a00df89e8b21de8
Zhila <ministrov2018@gmail.com>
2025-06-07 14:18:22 +0300

    fix - Fixing issues with a eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,11 +9,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// Assign config to a named variable
 const eslintConfig = [
   ...compat.extends(
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
     'prettier'
   ),
   {
@@ -39,47 +38,4 @@ const eslintConfig = [
   },
 ];
 
-// Export the named variable as default
 export default eslintConfig;
-
-// import { dirname } from 'path';
-// import { fileURLToPath } from 'url';
-// import { FlatCompat } from '@eslint/eslintrc';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [
-//   ...compat.extends(
-//     'next/core-web-vitals',
-//     'plugin:@typescript-eslint/recommended',
-//     'prettier'
-//   ),
-//   {
-//     ignores: ['**/*.d.ts', '.next/**/*', 'node_modules/**/*', 'dist/**/*'],
-//   },
-//   {
-//     files: ['**/*.ts', '**/*.tsx'],
-//     languageOptions: {
-//       parserOptions: {
-//         project: './tsconfig.json',
-//       },
-//     },
-//     rules: {
-//       semi: ['error', 'always'],
-//       quotes: ['error', 'single'],
-//       '@typescript-eslint/explicit-function-return-type': 'off',
-//       '@typescript-eslint/no-explicit-any': 'warn',
-//       '@typescript-eslint/no-unused-vars': [
-//         'warn',
-//         { argsIgnorePattern: '^_' },
-//       ],
-//     },
-//   }
-// ];
-
-// export default eslintConfig;
