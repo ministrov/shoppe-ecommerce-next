932457ee764b2c1efdd80795624dd1aa1b9854aa
ministrov <61511030+ministrov@users.noreply.github.com>
2025-06-21 14:14:50 +0300

    feature - Create a basic structure markup for searching component

--- components/searching/Searching.tsx
+++ components/searching/Searching.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import Image from 'next/image';
+import styles from './Searching.module.css';
 
-const Searching = () => {
+export const Searching = () => {
   return (
-    <div>Searching</div>
-  );
-};
-
-export default Searching;
+    <form className={styles.search}>
+      <Image src={'/search.svg'} width={12} height={12} alt={'Icon search'} />
+      <input className={styles.input} type="text" />
+    </form>
+  )
+}
