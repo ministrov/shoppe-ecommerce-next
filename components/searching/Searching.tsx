2fb7118f90cfa21a56e7b270121dd176f6a1fbc5
ministrov <61511030+ministrov@users.noreply.github.com>
2025-06-28 19:24:40 +0300

    fix - Fixing a searching component styles

--- components/searching/Searching.tsx
+++ components/searching/Searching.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image';
+/* eslint-disable @next/next/no-img-element */
 import styles from './Searching.module.css';
 
 export const Searching = () => {
   return (
     <form className={styles.search}>
-      <Image className={styles.searchIcon} src={'/search-icon-2.svg'} width={12} height={12} alt={'Icon search'} />
+      <img className={styles.searchIcon} width={12} height={12} src='/search-icon-2.svg' alt='Search' />
       <input className={styles.searchInput} type='text' name='search' placeholder='Поиск' />
     </form>
   );
