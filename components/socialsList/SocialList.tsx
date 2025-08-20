b4af201aee6c8f4322b318cb61d586a31df39da7
ministrov <61511030+ministrov@users.noreply.github.com>
2025-08-20 16:57:27 +0300

    feat: start creating a socials component add a markup and data structure

---
+++ components/socialsList/SocialList.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+// interface SocialListProps {}
+
+export const SocialList = () => {
+  return (
+    <ul>
+      <Link href='#'>{/* <Icon name="icons:li" size="25px" /> */}</Link>
+      {/* <Link to='#'><Icon name="icons:fb" size="25px" /></Link> */}
+      {/* <Link to='#'><Icon name="icons:in" size="25px" /></Link> */}
+      {/* <Link to='#'><Icon name="icons:tw" size="25px" /></Link> */}
+    </ul>
+  );
+};
---
+++ components/socialsList/socials.ts
@@ -0,0 +1,15 @@
+interface Social {
+  id: number,
+  icon: string,
+  url: string
+}
+
+export const socials: Social[] = [
+  {
+    id: 1, icon: '/in.svg',
+    url: "#"
+  },
+  { id: 2, icon: '/facebook.svg', url: '#' },
+  { id: 3, icon: '/insta.svg', url: '#' },
+  { id: 4, icon: '/twitter.svg', url: '#' },
+];
