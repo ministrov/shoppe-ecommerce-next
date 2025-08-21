fffa31301fb1c60933b61ca680a9514201dee5be
ministrov <61511030+ministrov@users.noreply.github.com>
2025-08-21 15:51:00 +0300

    fix: fixing a semantically adding a tag form for footer form subscribe

--- layouts/footer/Footer.tsx
+++ layouts/footer/Footer.tsx
@@ -21,10 +21,12 @@ export const Footer = () => {
           </li>
         </ul>
         <div className={styles.footer__form}>
-          <InputField className={styles.footer__input} placeholder='Ваш email для акций и предложений' />
-          <button className={styles.footer__subscribe}>
-            <Image src='/arrow-right.svg' width={20} height={20} alt='Subscribe icon' />
-          </button>
+          <form action='#' method='POST'>
+            <InputField className={styles.footer__input} placeholder='Ваш email для акций и предложений' />
+            <button className={styles.footer__subscribe} type='submit' name='subscribe'>
+              <Image src='/arrow-right.svg' width={20} height={20} alt='Subscribe icon' />
+            </button>
+          </form>
         </div>
       </div>
       <div className={styles.footer__bottom}>
