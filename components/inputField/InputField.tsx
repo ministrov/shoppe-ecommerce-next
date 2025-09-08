435b423f071f4ae76e3cda8eb7c58fdab4bc8576
Zhila <ministrov2018@gmail.com>
2025-09-08 16:04:41 +0300

    feat: creating a custorm thumb range styling

--- components/inputField/InputField.tsx
+++ components/inputField/InputField.tsx
@@ -2,8 +2,9 @@ import { InputFieldProps } from './InputField.interface';
 import cn from 'classnames';
 import styles from './InputField.module.css';
 
-export const InputField = ({ className, variant = 'black', ...props }: InputFieldProps) => {
+export const InputField = ({ className, variant, ...props }: InputFieldProps) => {
   return <input className={cn(styles.input, className, {
-    [styles.gray]: variant === 'gray'
+    [styles.gray]: variant === 'gray',
+    [styles.black]: variant === 'black'
   })} {...props} />;
 };
