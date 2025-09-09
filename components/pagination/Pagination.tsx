0d964b0fa3223055f1d5b765b729884909947634
Zhila <ministrov2018@gmail.com>
2025-09-09 17:42:46 +0300

    feat: add pagination basic styling

--- components/pagination/Pagination.tsx
+++ components/pagination/Pagination.tsx
@@ -1,26 +1,27 @@
+import Image from 'next/image';
 import { PaginationProps } from './Pagination.interface';
+import cn from 'classnames';
 import styles from './Pagination.module.css';
 
 export const Pagination = ({ page, total, onClick }: PaginationProps) => {
   return (
     <div className={styles.pagination}>
       {page > 1 && <button
-        className={styles.prevButton}
+        className={cn(styles.button, styles.prevButton)}
         onClick={() => onClick(page - 1)}
         disabled={page === 1}
-        style={{ margin: '0 10px', padding: '10px 20px' }}
       >
-        Назад
+        <Image src={'/next.svg'} width={6} height={10} alt={'Previous page button'} />
       </button>}
       <span style={{ margin: '0 10px', padding: '10px' }}>
         Страница {page} из {Math.ceil(total / 6)}
       </span>
       <button
+        className={cn(styles.button, styles.nextButton)}
         onClick={() => onClick(page + 1)}
         disabled={page >= Math.ceil(total / 6)} // Правильная логика отключения
-        style={{ margin: '0 10px', padding: '10px 20px' }}
       >
-        Вперед
+        <Image src={'/next.svg'} width={6} height={10} alt={'Next page button'} />
       </button>
     </div>
   )
