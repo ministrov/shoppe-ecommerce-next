f8e6445121e817d994e08ad76c76bffa01d55e76
Zhila <ministrov2018@gmail.com>
2025-09-10 16:40:51 +0300

    feat: create a basic markup structure of carousel component

--- components/carousel/Carousel.tsx
+++ components/carousel/Carousel.tsx
@@ -1,7 +1,12 @@
+import Image from 'next/image';
 import styles from './Carousel.module.css';
 
 export const Carousel = () => {
   return (
-    <div className={styles.carousel}>Carousel</div>
+    <div className={styles.carousel}>
+      <Image src={'/home-img-1.jpg'} width={1248} height={646} alt={'Image-1'} />
+      <Image src={'/home-img-2.jpg'} width={1248} height={646} alt={'Image-2'} />
+      <Image src={'/home-img-1.jpg'} width={1248} height={646} alt={'Image-3'} />
+    </div>
   )
 }
