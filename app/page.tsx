f22d4ea09dd630010415ea536e2f3f478e109fa8
Zhila <ministrov2018@gmail.com>
2025-09-10 14:18:49 +0300

    feat: start operating on home page creation, firstly to remove all unusual code on a page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,11 @@
-import GeolocationContainer from '@/components/geolocationContainer/GeolocationContainer';
-import { Button } from '@/components/button/Button';
-import { Searching } from '@/components/searching/Searching';
-// import { Message } from '@/components/message/Message';
+// import GeolocationContainer from '@/components/geolocationContainer/GeolocationContainer';
+// import { Button } from '@/components/button/Button';
+// import { Searching } from '@/components/searching/Searching';
 
 export default function Home() {
   return (
     <div>
-      <GeolocationContainer />
-
-      <div style={{ maxWidth: '500px' }}>
-        <Button>Вход</Button>
-        <Button size={'medium'} ghost>
-          Вход
-        </Button>
-        <Button size={'small'}>Отправить</Button>
-      </div>
-
-      <h1>
-        Hello world!
-      </h1>
-
-      <Searching />
-
-      {/* <Message content={'dfdfdfdfde'} /> */}
+      Home Page
     </div>
   );
 }
