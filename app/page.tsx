c5f343f9623322296d2adb8fb6781bef7c0db971
Zhila <ministrov2018@gmail.com>
2025-09-10 16:27:23 +0300

    fix: fixing a styling of searching component to increasing an accessibility and usability

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link';
+import { Searching } from '@/components/searching/Searching';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <section>
+      <div className={styles.searchMobile}>
+        <Searching />
+      </div>
       <div className={styles.carousel}>Carousel</div>
 
       <section className={styles.newIncome}>
