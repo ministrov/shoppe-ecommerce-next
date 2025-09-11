faf6a4da75b06eafc745b64d4168f5e6b8bff141
Zhila <ministrov2018@gmail.com>
2025-09-11 10:13:42 +0300

    feat: create a markup of the carousel structure template

--- components/carousel/Carousel.tsx
+++ components/carousel/Carousel.tsx
@@ -1,15 +1,45 @@
 import Image from 'next/image';
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { CarouselProps } from './Carousel.interface';
 import styles from './Carousel.module.css';
 
 export const Carousel = ({ images, autoPlayInterval = 5000 }: CarouselProps) => {
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
+
   console.log(images, autoPlayInterval);
+  console.log(currentSlide, setCurrentSlide);
+
+  useEffect(() => { }, []);
   return (
     <div className={styles.carousel}>
-      <Image src={'/home-img-1.jpg'} width={1248} height={646} alt={'Image-1'} />
-      <Image src={'/home-img-2.jpg'} width={1248} height={646} alt={'Image-2'} />
-      <Image src={'/home-img-1.jpg'} width={1248} height={646} alt={'Image-3'} />
+      <div className={styles.slidesContainer}>
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`${styles.slide} ${index === currentSlide ? styles.active : ''}`}
+          >
+            <Image
+              src={image.src}
+              width={image.width}
+              height={image.height}
+              alt={image.alt}
+              className={styles.image}
+            />
+          </div>
+        ))}
+      </div>
+
+      {/* Буллеты */}
+      <div className={styles.bullets}>
+        {images.map((_, index) => (
+          <button
+            key={index}
+            className={`${styles.bullet} ${index === currentSlide ? styles.active : ''}`}
+            // onClick={() => goToSlide(index)}
+            aria-label={`Перейти к слайду ${index + 1}`}
+          />
+        ))}
+      </div>
     </div>
   )
 }
