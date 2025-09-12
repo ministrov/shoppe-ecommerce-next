ec296de51c29b7f82e384226567e328bc7c97e3e
Zhila <ministrov2018@gmail.com>
2025-09-12 16:29:01 +0300

    feat: render a favoritesCount data to header userMenu

--- components/userMenu/UserMenu.tsx
+++ components/userMenu/UserMenu.tsx
@@ -1,5 +1,5 @@
 import { ButtonLink } from '../buttonLink/ButtonLink';
-// import { useFavorites } from '@/hooks/useFavorite';
+import { useFavorites } from '@/hooks/useFavorite';
 import styles from './UserMenu.module.css';
 
 type UserMenuProps = {
@@ -7,24 +7,21 @@ type UserMenuProps = {
 };
 
 export const UserMenu = ({ pathname }: UserMenuProps) => {
-  console.log(pathname)
-  // const { selectFavoritesCount } = useFavorites();
-  // const links = [
-  //   { id: 1, path: '/cart', text: '', iconPath: '/shopping-cart.svg', isCount: true, count: 0 },
-  //   { id: 2, path: '/favorites', text: '', iconPath: '/heart.svg', isCount: true, count: selectFavoritesCount || 0 },
-  //   { id: 3, path: '/orders', text: '', iconPath: '/account.svg' },
-  // ];
+  const { favoritesCount } = useFavorites();
+
+  const links = [
+    { id: 1, path: '/cart', text: '', iconPath: '/shopping-cart.svg', isCount: true, count: 0 },
+    { id: 2, path: '/favorites', text: '', iconPath: '/heart.svg', isCount: true, count: favoritesCount || 0 },
+    { id: 3, path: '/orders', text: '', iconPath: '/account.svg' },
+  ];
+
   return (
     <ul className={styles.userMenu}>
-      <li className={styles.userItem}>
-        <ButtonLink pathname={'/cart'} path={'/cart'} iconPath={'/shopping-cart.svg'} isCount count={1} />
-      </li>
-      <li className={styles.userItem}>
-        <ButtonLink pathname={'/favorites'} path={'/favorites'} iconPath={'/heart.svg'} isCount count={0} />
-      </li>
-      <li className={styles.userItem}>
-        <ButtonLink pathname={'/orders'} path={'/orders'} iconPath={'/account.svg'} isCount count={0} />
-      </li>
+      {links.map(link => (
+        <li key={link.id} className={styles.userItem}>
+          <ButtonLink pathname={pathname} path={link.path} iconPath={link.iconPath} isCount={link.isCount} count={link.count} />
+        </li>
+      ))}
     </ul>
   );
 };
