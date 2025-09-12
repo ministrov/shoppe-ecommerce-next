74f1d7fc411beae3a35c508d1c55e096080b4db2
Zhila <ministrov2018@gmail.com>
2025-09-12 16:49:10 +0300

    feat: create a noFavorites component

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+
+import { NoFavorites } from '@/components/noFavorites/NoFavorites';
+import { useFavorites } from '@/hooks/useFavorite';
 import styles from './page.module.css';
 
 export default function Favorites() {
+  const { favoritesCount } = useFavorites();
+
   return <div className={styles.favorites}>
     <h1 className="left">Избранное</h1>
+
+    {favoritesCount === 0 && <NoFavorites />}
   </div>;
 };
