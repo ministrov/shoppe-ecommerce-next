34ef402845a051bd328038b4ec964ad5f970332b
Zhila <ministrov2018@gmail.com>
2025-09-16 12:21:45 +0300

    fix: fixing a preload problem in logo component

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -7,7 +7,7 @@ export const Logo = () => {
   return (
     <div className={styles.logo}>
       <Link href={'/'} >
-        <Image src={'/SHOPPE.svg'} width={158} height={40} alt={'logo'} priority/>
+        <Image src={'/SHOPPE.svg'} width={158} height={40} alt={'logo'} />
       </Link>
     </div>
   );
