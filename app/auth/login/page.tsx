aa73fc12cc29e4b5a37d5597c303d8d5cc9a0ec1
Zhila <ministrov2018@gmail.com>
2025-09-16 12:40:39 +0300

    feat: create a markup for login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,3 +1,26 @@
+import { InputField } from '@/components/inputField/InputField';
+import styles from './page.module.css';
+import { Button } from '@/components/button/Button';
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className={styles.login}>
+      <h1>Мой аккаунт</h1>
+
+      <form method="post" action="" className={styles.form}>
+        <div className={styles.fiels}>
+          <InputField variant="gray" placeholder="Email" />
+          <InputField
+            type="password"
+            variant="gray"
+            placeholder="Пароль"
+          />
+        </div>
+
+        <Button color="primary">
+          Вход
+        </Button>
+      </form>
+    </div>
+  );
 }
