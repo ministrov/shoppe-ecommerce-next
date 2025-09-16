0a124f536d5b305d7724e9923408ce07e9ff7816
Zhila <ministrov2018@gmail.com>
2025-09-16 14:31:01 +0300

    feat: create a basic styles for tabs and tabItem on login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,16 +1,38 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { InputField } from '@/components/inputField/InputField';
 import styles from './page.module.css';
 import { Button } from '@/components/button/Button';
 
 export default function Login() {
+  const pathname = usePathname();
+
+  const tabs = [
+    { href: '/auth/login', label: 'Войти' },
+    { href: '/auth/register', label: 'Зарегистрироваться' },
+  ];
   return (
     <div className={styles.login}>
       <h1>Мой аккаунт</h1>
 
       <div className={styles.tabs}>
-        <Link href={'/auth/login'}>Войти</Link>
-        <Link href={'/auth/register'}>Зарегистрироваться</Link>
+        {/* <span className={styles.tabsItem}>
+          <Link href={'/auth/login'}>Войти</Link>
+        </span>
+        <span className={styles.tabsItem}>
+          <Link href={'/auth/register'}>Зарегистрироваться</Link>
+        </span> */}
+        {tabs.map((tab) => (
+          <span
+            key={tab.href}
+            className={`${styles.tabsItem} ${pathname === tab.href ? styles.active : ''
+              }`}
+          >
+            <Link href={tab.href}>{tab.label}</Link>
+          </span>
+        ))}
       </div>
 
       <form method="post" action="" className={styles.form}>
@@ -22,6 +44,11 @@ export default function Login() {
             placeholder="Пароль"
           />
         </div>
+
+        <label htmlFor="rememberMe">
+          <input type="checkbox" id="rememberMe" />
+          Запомнить меня
+        </label>
 
         <Button color="primary">
           Вход
