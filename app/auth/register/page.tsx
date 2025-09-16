328e0ea3fc6252ad3f178b8a043e69a70cfd4b77
Zhila <ministrov2018@gmail.com>
2025-09-16 12:56:28 +0300

    feat: add a basic styles for login page

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,3 +1,15 @@
+import Link from 'next/link';
+import styles from './page.module.css';
+
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <div className={styles.register}>
+      <h1>Мой аккаунт</h1>
+
+      <div className={styles.tabs}>
+        <Link href={'/auth/login'}>Войти</Link>
+        <Link href={'/auth/register'}>Зарегистрироваться</Link>
+      </div>
+    </div>
+  );
 }
