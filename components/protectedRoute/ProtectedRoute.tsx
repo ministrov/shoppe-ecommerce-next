14947e4a56c201c0076be7bf3702ca6c715e4372
Zhila <ministrov2018@gmail.com>
2025-09-17 14:53:40 +0300

    feat: create a protectedRoute component

---
+++ components/protectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useAppSelector } from '@/store/hooks';
+import { useEffect } from 'react';
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const { token, isLoading } = useAppSelector((state) => state.auth);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && !token) {
+      router.push('/auth/login');
+    }
+  }, [token, isLoading, router]);
+
+  if (isLoading) {
+    return (
+      <div className="loading-container">
+        <div>Загрузка...</div>
+      </div>
+    );
+  }
+
+  if (!token) {
+    return null; // или лоадер, пока происходит редирект
+  }
+
+  return <>{children}</>;
+}
