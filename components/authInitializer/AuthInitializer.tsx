b692e9aaca9ef23a1ccd9d23c7555868b9dae17f
Zhila <ministrov2018@gmail.com>
2025-09-18 10:38:52 +0300

    refactore: create authInitializer component

---
+++ components/authInitializer/AuthInitializer.tsx
@@ -0,0 +1,16 @@
+// components/AuthInitializer.tsx
+'use client';
+
+import { useEffect } from 'react';
+import { useAppDispatch } from '@/store/hooks';
+import { initializeAuth } from '@/store/features/auth/authSlice';
+
+export function AuthInitializer() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(initializeAuth());
+  }, [dispatch]);
+
+  return null;
+}
