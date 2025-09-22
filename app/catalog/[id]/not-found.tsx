75a1ad4e4e43e41c2039384c8ae474a3f1da1fdc
Zhila <ministrov2018@gmail.com>
2025-09-22 14:45:13 +0300

    fix: fixing a not found component for dynamic routes in catalog folder

--- app/catalog/[id]/not-found.tsx
+++ app/catalog/[id]/not-found.tsx
@@ -1,9 +1,12 @@
-'use client';
+// 'use client';
 
-import { PageNotFound } from '@/components/pageNotFound/pageNotFound';
+// import { PageNotFound } from '@/components/pageNotFound/pageNotFound';
 
-export default function NotFound() {
-  return (
-    <PageNotFound />
-  )
+// export default function NotFound() {
+//   return (
+//     <PageNotFound />
+//   )
+// }
+export default function TestNotFound() {
+  return <div>Test Not Found Page - Working</div>
 }
--- app/catalog/[id]/page.tsx
+++ app/catalog/[id]/page.tsx
@@ -1,8 +1,14 @@
+// import { notFound } from 'next/navigation'
+
 type ProductPageProps = {
   params: Promise<{ id: string }>
 }
 
 export default async function ProductPage({ params }: ProductPageProps) {
+  // if (!(await params).id) {
+  //   notFound();
+  // }
+
   return (
     <div>My product: {(await params).id}</div>
   )
