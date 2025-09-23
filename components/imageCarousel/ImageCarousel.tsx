7d5accbbb0c2947b900b543767db82300c3b2f3c
Zhila <ministrov2018@gmail.com>
2025-09-23 15:01:33 +0300

    refactore: rebuld a thumbnail gallery for more apropriate way to show a bigger image for user clicking thumbnail picture

--- components/imageCarousel/ImageCarousel.tsx
+++ components/imageCarousel/ImageCarousel.tsx
@@ -1,23 +1,56 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 import { ImageCarouselProps } from './ImageCarousel.interface';
 import styles from './ImageCarousel.module.css';
 
 export const ImageCarousel = ({ images }: ImageCarouselProps) => {
+  const [selectedImageIndex, setSelectedImageIndex] = useState<number>(0);
+
+  if (!images.length) {
+    return <div className={styles.emptyState}>No images available</div>;
+  }
+
+  const mainImage = images[selectedImageIndex];
+
   return (
-    <ul className={styles.imgCarousel}>
-      {images.map((image, index) => (
-        <li
-          key={index}
-          className={`${styles.imgCarouselItem} ${index === 0 ? 'active' : ''}`}
-        >
-          <Image
-            src={image}
-            alt={`Product image ${index + 1}`}
-            width={index === 0 ? 570 : 120}
-            height={index === 0 ? 630 : 120}
-          />
-        </li>
-      ))}
-    </ul>
+    <div className={styles.carouselContainer}>
+      {/* Основное большое изображение */}
+      <div className={styles.mainImageWrapper}>
+        <Image
+          src={mainImage}
+          alt={`Product image ${selectedImageIndex + 1}`}
+          width={570}
+          height={630}
+          className={styles.mainImage}
+          priority
+        />
+      </div>
+
+      {/* Галерея превью */}
+      {images.length > 1 && (
+        <div className={styles.thumbnailGallery}>
+          {images.map((image, index) => (
+            <button
+              key={index}
+              className={`${styles.thumbnail} ${index === selectedImageIndex ? styles.active : ''
+                }`}
+              onClick={() => setSelectedImageIndex(index)}
+              aria-label={`View image ${index + 1}`}
+              type="button"
+            >
+              <Image
+                src={image}
+                alt={`Thumbnail ${index + 1}`}
+                width={120}
+                height={120}
+                className={styles.thumbnailImage}
+              />
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
   )
 }
