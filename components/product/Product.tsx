7e6b90612ecf41b0c77204e16b5d8b492bae33a4
Zhila <ministrov2018@gmail.com>
2025-09-23 10:38:59 +0300

    feat: keep on operating on creating a carousel of images

--- components/product/Product.tsx
+++ components/product/Product.tsx
@@ -1,13 +1,36 @@
+import Image from 'next/image';
 import { ProductProps } from './Product.interface';
 import styles from './Product.module.css';
 
 export const Product = ({ product }: ProductProps) => {
+  // const { product } = product;
   return (
     <article className={styles.product}>
-      <div className="card__img-container">
-
+      <div className={styles.imgContainer}>
+        <ul className="img-carousel">
+          <li className="img-carousel__item active">
+            {product?.product.images[0] && (
+              <Image src={product?.product.images[0]} alt="" width="570" height="630" />
+            )}
+          </li>
+          <li className="img-carousel__item">
+            {product?.product.images[1] && (
+              <Image src={product?.product.images[1]} alt="" width="170" height="160" />
+            )}
+          </li>
+          <li className="img-carousel__item">
+            {product?.product.images[2] && (
+              <Image src={product?.product.images[2]} alt="" width="170" height="160" />
+            )}
+          </li>
+          <li className="img-carousel__item">
+            {product?.product.images[3] && (
+              <Image src={product?.product.images[3]} alt="" width="170" height="160" />
+            )}
+          </li>
+        </ul>
       </div>
-      <div className="card__info-container card-info">
+      <div className={styles.infoContainer}>
         <h1>Product Page</h1>
         <p>Product ID: {product?.product.id}</p>
       </div>
