81b2f05aaca8ceb4848585fb7e68193401f228ec
Zhila <ministrov2018@gmail.com>
2025-10-16 13:48:14 +0300

    feat: Add a basic grid styles to cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,17 +2,17 @@
 
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-import { Button } from '@/components/button/Button';
+// import { Button } from '@/components/button/Button';
 import { useAuth } from '@/hooks/useAuth';
-import { incCount, decCount } from '@/store/features/counter/counterSlice';
-import { useAppDispatch, useAppSelector } from '@/store/hooks';
+// import { incCount, decCount } from '@/store/features/counter/counterSlice';
+// import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import styles from './page.module.css';
 
 export default function Cart() {
   const router = useRouter();
   const { isAuthenticated } = useAuth();
-  const counter = useAppSelector((state) => state.counter.counter);
-  const dispatch = useAppDispatch();
+  // const counter = useAppSelector((state) => state.counter.counter);
+  // const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (!isAuthenticated) {
@@ -24,21 +24,9 @@ export default function Cart() {
     <div className={styles.cart}>
       <h1>Корзина</h1>
 
-      <div></div>
-      <div></div>
-
-      <div>
-        Cart
-
-        <Button onClick={() => dispatch(incCount())}>
-          +
-        </Button>
-
-        {counter}
-
-        <Button onClick={() => dispatch(decCount())}>
-          -
-        </Button>
+      <div className={styles.wrapper}>
+        <div className={styles.cartItem}></div>
+        <div className={styles.info}></div>
       </div>
     </div>
   );
