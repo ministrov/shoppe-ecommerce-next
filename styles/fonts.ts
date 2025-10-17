3264f5f95523825d28cc573196e321d810cf678c
Zhila <ministrov2018@gmail.com>
2025-10-17 11:21:06 +0300

    fix: find and fix a bug in global css with a font connection

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,9 +1,9 @@
-import { DM_Sans } from "next/font/google"
+import { DM_Sans } from 'next/font/google';
 
 // Configure DM Sans with multiple weights
 export const dmSans = DM_Sans({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "700"],
-  display: "swap",
-  variable: "--font-dm-sans",
-})
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],
+  display: 'swap',
+  variable: '--font-dm-sans-dm-sans',
+});
