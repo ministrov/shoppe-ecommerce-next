f70c9ff80af7821e60e73250edd76c2f03d134a6
Zhila <ministrov2018@gmail.com>
2025-10-27 18:22:43 +0300

    fix: fixing a structure of mockProductCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link from 'next/link';
-// import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { Carousel } from '@/components/carousel/Carousel';
 import { ProductCard } from '@/components/productCard/ProductCard';
--- components/mockProductCard/MockProductCard.tsx
+++ components/mockProductCard/MockProductCard.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
 import { MockProductCardProps } from './MockProductCard.interface';
-// import { MockProduct } from '@/helpers';
+import styles from './MockProductCard.module.css';
 
 export const MockProductCard = ({ product, index }: MockProductCardProps) => {
   return (
-    <li key={product.id}>
+    <li className={styles.item} key={product.id}>
       <Image src={product.images[index]} width={377} height={380} alt='' />
 
       <p>{product.title}</p>
