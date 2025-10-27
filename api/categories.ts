b3311b6ab5989dcad876326b1db2771edc0e20cf
Zhila <ministrov2018@gmail.com>
2025-10-27 16:11:54 +0300

    fix: fixing a bug occuring in api product, products, categories and add a checking response is ok

--- api/categories.ts
+++ api/categories.ts
@@ -4,6 +4,13 @@ import { GetCategoryResponse } from '@/interfaces/category.interface';
 export async function getCategories() {
   try {
     const response = await fetch(API_URL + '/categories');
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch categories: ${response.status} ${response.statusText}`
+      );
+    }
+
     const data: GetCategoryResponse | undefined = await response.json();
     return data?.categories;
   } catch (error) {
--- api/products.ts
+++ api/products.ts
@@ -4,6 +4,12 @@ import { GetProductsResponse } from '@/interfaces/product.interface';
 export async function getProducts() {
   try {
     const response = await fetch(API_URL + '/products');
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch products: ${response.status} ${response.statusText}`
+      );
+    }
     const data: GetProductsResponse = await response.json();
 
     return data.products;
