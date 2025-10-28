0e1770bf2af6110497b95c4635c6dc53930af26a
Zhila <ministrov2018@gmail.com>
2025-10-28 16:40:01 +0300

    refactore: refactor homepage to use Next.js Server Components and loading indicator for data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,38 @@
-'use client';
-
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import { Carousel } from '@/components/carousel/Carousel';
 import { ProductCard } from '@/components/productCard/ProductCard';
 import { Searching } from '@/components/searching/Searching';
 import { MockProductCard } from '@/components/mockProductCard/MockProductCard';
 import { getProducts } from '@/api/products';
-import { Product } from '@/interfaces/product.interface';
+// import { Product } from '@/interfaces/product.interface';
 import { carouselImages } from '@/interfaces/carousel.interface';
 import { demoProducts } from '@/helpers';
 import styles from './page.module.css';
 
-export default function Home() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+export default async function Home() {
+  // const [products, setProducts] = useState<Product[]>([]);
+  // const [isLoading, setIsLoading] = useState<boolean>(false);
+  const products = await getProducts();
 
-  useEffect(() => {
-    const getLastIncomeProducts = async () => {
-      setIsLoading(true);
-      try {
-        const incomeProducts = await getProducts();
+  // useEffect(() => {
+  //   const getLastIncomeProducts = async () => {
+  //     setIsLoading(true);
+  //     try {
+  //       const incomeProducts = await getProducts();
 
-        if (incomeProducts) {
-          setProducts(incomeProducts);
-        }
-      } catch (err) {
-        console.error('Error loading incomeProducts:', err);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+  //       if (incomeProducts) {
+  //         setProducts(incomeProducts);
+  //       }
+  //     } catch (err) {
+  //       console.error('Error loading incomeProducts:', err);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   };
 
-    getLastIncomeProducts();
-  }, []);
+  //   getLastIncomeProducts();
+  // }, []);
 
   return (
     <section>
@@ -54,16 +53,16 @@ export default function Home() {
           <Link href={'/catalog'} >Все</Link>
         </header>
 
-        {isLoading && <div className={styles.loading}>Loading...</div>}
+        {/* {isLoading && <div className={styles.loading}>Loading...</div>} */}
 
         <ul className={styles.list}>
-          {!isLoading && products.length === 0 && demoProducts.map((product) => (
+          {products?.length === 0 && demoProducts.map((product) => (
             <MockProductCard key={product.id} product={product} />
           ))}
         </ul>
 
         <ul className={styles.list}>
-          {!isLoading && products.length > 0 && products?.slice(0, 6).map(product => (
+          {products?.slice(0, 6).map(product => (
             <ProductCard key={product.id} product={product} />
           ))}
         </ul>
