bc48f635226ae5aeaf9ab144827c2e1f0f1e4807
Zhila <ministrov2018@gmail.com>
2025-11-21 13:14:21 +0300

    feat: add a basic styling for toggle component like as figma

--- components/toggle/Toggle.tsx
+++ components/toggle/Toggle.tsx
@@ -2,6 +2,8 @@ import styles from './Toggle.module.css';
 
 export const Toggle = () => {
   return (
-    <div className={styles.toggle}>Toggle</div>
+    <div className={styles.toggle}>
+      <div className={styles.thumb}></div>
+    </div>
   )
 }
